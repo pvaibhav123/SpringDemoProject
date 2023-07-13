17144909f9ea07d5a4d8ebc1fdd8e6392edc2a83
Vaibhav <vaibhavpanwalkar39@gmail.com>
2023-07-13 09:35:00 +0530

    springdemo2 project add

---
+++ src/main/java/com/example/demo/SpringWebDemoProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDemoProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebDemoProject2Application.class, args);
+	}
+
+}
